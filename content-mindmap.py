f975168235f38884509b92c5e9f7c1f1c32fcbde
SilverStreakzz <kybrutnell@gmail.com>
2023-02-17 11:44:23 +1000

    def get_internal_links()

--- content-mindmap.py
+++ content-mindmap.py
@@ -9,3 +9,18 @@ class WebsiteContentMap:
     """
     def __init__(self, url):
         self.url = url
+    
+    def get_url(self):
+        return self.url
+    
+    def get_internal_links(self):
+        url = self.get_url()
+        links = []
+        r = requests.get(url)
+        print(f'Getting links from {url}...')
+        soup = BeautifulSoup(r.content, 'html.parser')
+        for link in soup.find_all('a'):
+            href = link.get('href')
+            if href is not None and urlparse(href).netloc == urlparse(url).netloc:
+                links.append(href)
+        return links
