0af16d36494f8bec540577bd958839c2acafd6f1
SilverStreakzz <kybrutnell@gmail.com>
2023-02-17 11:44:52 +1000

    def create_mind_map()

--- content-mindmap.py
+++ content-mindmap.py
@@ -24,3 +24,23 @@ class WebsiteContentMap:
             if href is not None and urlparse(href).netloc == urlparse(url).netloc:
                 links.append(href)
         return links
+    
+    def create_mind_map():
+        url = self.get_url()
+        g = gt.Graph(directed=True)
+        vprop = g.new_vertex_property("string")
+        root_url = urlparse(url).scheme + '://' + urlparse(url).netloc
+        v = g.add_vertex()
+        vprop[v] = root_url
+        visited = set([root_url])
+        queue = [root_url]
+        while queue:
+            current_url = queue.pop(0)
+            for link in get_internal_links(current_url):
+                if link not in visited:
+                    visited.add(link)
+                    queue.append(link)
+                    v2 = g.add_vertex()
+                    vprop[v2] = urlparse(link).path
+                    g.add_edge(v, v2)
+        gt.graph_draw(g, vertex_text=vprop, vertex_font_size=18, output='mindmap.png')
