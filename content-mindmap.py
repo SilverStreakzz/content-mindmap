0891f3378df875789171dc91541b8e62787d5881
Ky <kybrutnell@gmail.com>
2023-02-22 15:43:09 +1000

    add to create_nodes()

--- content-mindmap.py
+++ content-mindmap.py
@@ -13,12 +13,70 @@ class WebsiteMap:
         self.url = url
         self.nodes = {}
         self.create_nodes()
+    
+    def create_links(self, url):
+        """ Create links.
+        
+        Creates a list of links for the current url.
+        """
+        links = []
+        r = requests.get(url)
+        soup = BeautifulSoup(r.content, 'html.parser')
+        for link in soup.find_all('a'):
+            href = link.get('href')
+            if href:
+                link_domain = urlparse(href).netloc
+                if link_domain == urlparse(url).netloc:
+                    links.append(href)
+        return links
+    
+    def filter_links(self, links):
+        """ Filter links.
+        
+        Filters the list of links to remove any that are not a part of the main website url.
+        """
+        url = get_url()
+        filtered_links = []
+        for link in links:
+            if link.startswith(url):
+                filtered_links.append(link)
+
+        return filtered_links
+    
+    def create_link_dict(self):
+        """ Create link dictionary.
+
+        Creates a dictionary of links, where each key is a url and the value is a list of links.
+
+        Sample output:
+            {
+                "https://aginic.com/blog/": 
+                [
+                    "https://aginic.com/blog/what-is-a-mind-map/",
+                    "https://aginic.com/blog/analytics/",
+                    "https://aginic.com/blog/machine-learning/"
+                    ],
+                "https://aginic.com/blog/what-is-a-mind-map/":
+                [
+                    "https://aginic.com/blog/",
+                    ],
+                "https://aginic.com/blog/analytics/": [],
+                "https://aginic.com/blog/machine-learning/": []
+                }
+        """
+        url = self.get_url()
+        links = self.create_links(url)
+        link_dict = {}
+        for link in links:
+            link_dict[link] = self.create_links(link)
+        
+        return link_dict
 
     def create_nodes(self):
         """ Create nodes.
 
         Creates a dictionary of nodes, where each node is a url and the value is a dictionary of inbound and outbound links.
-        This dictionary is then set to self.nodes.
+        This dictionary is then set to self.nodes.  
 
         Sample output:
             {
@@ -32,13 +90,18 @@ class WebsiteMap:
                 }
             }
         """
-        links = {}
-        r = requests.get(self.url)
-        soup = BeautifulSoup(r.content, 'html.parser')
+        link_dict = self.create_link_dict()
+        node_dict = {}
+        for key, value in link_dict.items():
+            node_dict[key] = {
+                "inbound": [],
+                "outbound": value
+            }
+        for key, value in link_dict.items():
+            if key in node_dict:
 
-        # Write code here to create a dictionary of nodes, where each node is a url and the value is a dictionary of inbound and outbound links
 
-        self.set_nodes(links)
+        self.set_nodes(nodes)
 
     def get_nodes(self) -> dict:
         return self.nodes
@@ -63,3 +126,5 @@ class WebsiteMap:
 
 
 aginic = WebsiteMap('https://aginic.com')
+nodes = aginic.get_nodes()
+print(nodes)
